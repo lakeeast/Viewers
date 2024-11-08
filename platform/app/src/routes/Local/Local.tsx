e6cee89f7bef9ac5b3b0b5b596cee8615328a8bc
Yuanming Chen <yuanming@gmail.com>
2024-11-07 21:07:13 -0700

    ROUT-117 Open two windows

--- platform/app/src/routes/Local/Local.tsx
+++ platform/app/src/routes/Local/Local.tsx
@@ -2,40 +2,38 @@ import React, { useEffect, useState, useRef } from 'react';
 import classnames from 'classnames';
 import { useNavigate } from 'react-router-dom';
 import { DicomMetadataStore, MODULE_TYPES } from '@ohif/core';
-
-import Dropzone from 'react-dropzone';
+import Dropzone, { DropzoneRef } from 'react-dropzone';
 import filesToStudies from './filesToStudies';
-
 import { extensionManager } from '../../App.tsx';
-
 import { Icon, Button, LoadingIndicatorProgress } from '@ohif/ui';
 
-const getLoadButton = (onDrop, text, isDir) => {
+const fetchBlobs = async (objectUrls: string[]) => {
+  return await Promise.all(
+    objectUrls.map(async (url: string) => {
+      const response = await fetch(url);
+      const blob = await response.blob();
+      return blob;
+    })
+  );
+};
+
+const getLoadButton = (onDrop: any, text: string, isDir: boolean) => {
   return (
-    <Dropzone
-      onDrop={onDrop}
-      noDrag
-    >
+    <Dropzone onDrop={onDrop} noDrag>
       {({ getRootProps, getInputProps }) => (
         <div {...getRootProps()}>
           <Button
             rounded="full"
-            variant="contained" // outlined
+            variant="contained"
             disabled={false}
-            endIcon={<Icon name="launch-arrow" />} // launch-arrow | launch-info
+            endIcon={<Icon name="launch-arrow" />}
             className={classnames('font-medium', 'ml-2')}
-            onClick={() => {}}
           >
             {text}
-            {isDir ? (
-              <input
-                {...getInputProps()}
-                webkitdirectory="true"
-                mozdirectory="true"
-              />
-            ) : (
-              <input {...getInputProps()} />
-            )}
+            <input
+              {...getInputProps()}
+              {...(isDir ? { directory: '', webkitdirectory: '' } : {})}
+            />
           </Button>
         </div>
       )}
@@ -49,38 +47,33 @@ type LocalProps = {
 
 function Local({ modePath }: LocalProps) {
   const navigate = useNavigate();
-  const dropzoneRef = useRef();
+  const dropzoneRef = useRef<DropzoneRef>(null);
   const [dropInitiated, setDropInitiated] = useState(false);
 
   // Initializing the dicom local dataSource
   const dataSourceModules = extensionManager.modules[MODULE_TYPES.DATA_SOURCE];
-  const localDataSources = dataSourceModules.reduce((acc, curr) => {
-    const mods = [];
-    curr.module.forEach(mod => {
-      if (mod.type === 'localApi') {
-        mods.push(mod);
-      }
-    });
+  const localDataSources = dataSourceModules.reduce((acc: any[], curr: any) => {
+    const mods = curr.module.filter((mod: any) => mod.type === 'localApi');
     return acc.concat(mods);
   }, []);
-
   const firstLocalDataSource = localDataSources[0];
-  const dataSource = firstLocalDataSource.createDataSource({});
+  const dataSource = firstLocalDataSource?.createDataSource({});
 
   const microscopyExtensionLoaded = extensionManager.registeredExtensionIds.includes(
     '@ohif/extension-dicom-microscopy'
   );
 
-  const onDrop = async (acceptedFiles) => {
+  const onDrop = async (acceptedFiles: any[]) => {
     const studies = await filesToStudies(acceptedFiles, dataSource);
-
     const query = new URLSearchParams();
 
     if (microscopyExtensionLoaded) {
-      const smStudies = studies.filter(id => {
+      const smStudies = studies.filter((id: string) => {
         const study = DicomMetadataStore.getStudy(id);
         return (
-          study.series.findIndex(s => s.Modality === 'SM' || s.instances[0].Modality === 'SM') >= 0
+          study.series.findIndex(
+            (s: any) => s.Modality === 'SM' || s.instances[0].Modality === 'SM'
+          ) >= 0
         );
       });
 
@@ -92,34 +85,35 @@ function Local({ modePath }: LocalProps) {
 
     studies.forEach(id => query.append('StudyInstanceUIDs', id));
     query.append('datasources', 'dicomlocal');
-
-    navigate(`/${modePath}?${decodeURIComponent(query.toString())}`);
+    navigate(`/${modePath}?${query.toString()}`);
   };
 
-  // Set body style
+  // Handle messages and viewerReady logic
   useEffect(() => {
-    document.body.classList.add('bg-black');
-    return () => {
-      document.body.classList.remove('bg-black');
-    };
-  }, []);
-
-  // Step 1: Send 'viewerReady' message to the parent (Angular) window when the React page is loaded
-  useEffect(() => {
-    // Send the 'viewerReady' message to the parent window
-    window.top.postMessage({type: 'ohifReady'}, '*');
-    const handlePostMessage = (event) => {
-      const blobs = event.data;
-      onDrop(blobs);
+    const handlePostMessage = async (event: MessageEvent) => {
+      if (event.data.type !== 'ohifReady') {
+        const blobs = event.data;
+        const objectUrls = blobs.map((blob: Blob) => URL.createObjectURL(blob));
+        localStorage.setItem('ohifBlobCollection', JSON.stringify(objectUrls));
+        const fullUrl = `${window.location.origin}/local?nativeViewer=true`;
+        setTimeout(() => window.open(fullUrl, '_blank'), 1000);
+      }
     };
 
-    // Add event listener for postMessage
-    window.addEventListener('message', handlePostMessage);
+    const urlParams = new URLSearchParams(window.location.search);
+    const nativeViewer = urlParams.get('nativeViewer');
 
-    // Cleanup on component unmount
-    return () => {
-      window.removeEventListener('message', handlePostMessage);
-    };
+    if (nativeViewer === 'true') {
+      const objectUrls = JSON.parse(localStorage.getItem('ohifBlobCollection') || '[]');
+      if (objectUrls.length > 0) {
+        fetchBlobs(objectUrls).then(onDrop);
+      }
+    } else {
+      window.addEventListener('message', handlePostMessage);
+      window.top.postMessage({ type: 'ohifReady' }, '*');
+    }
+
+    return () => window.removeEventListener('message', handlePostMessage);
   }, []);
 
   return (
@@ -132,37 +126,22 @@ function Local({ modePath }: LocalProps) {
       noClick
     >
       {({ getRootProps }) => (
-        <div
-          {...getRootProps()}
-          style={{ width: '100%', height: '100%' }}
-        >
-          <div className="flex h-screen w-screen items-center justify-center ">
+        <div {...getRootProps()} style={{ width: '100%', height: '100%' }}>
+          <div className="flex h-screen w-screen items-center justify-center">
             <div className="bg-secondary-dark mx-auto space-y-2 rounded-lg py-8 px-8 drop-shadow-md">
-              <div className="flex items-center justify-center">
-                <Icon
-                  name="logo-dark-background"
-                  className="h-28"
-                />
-              </div>
-              <div className="space-y-2 pt-4 text-center">
-                {dropInitiated ? (
-                  <div className="flex flex-col items-center justify-center pt-48">
-                    <LoadingIndicatorProgress className={'h-full w-full bg-black'} />
-                  </div>
-                ) : (
-                  <div className="space-y-2">
-                    <p className="text-base text-blue-300">
-                      Note: You data is not uploaded to any server, it will stay in your local
-                      browser application
-                    </p>
-                    <p className="text-xg text-primary-active pt-6 font-semibold">
-                      Drag and Drop DICOM files here to load them in the Viewer
-                    </p>
-                    <p className="text-lg text-blue-300">Or click to </p>
-                  </div>
-                )}
-              </div>
-              <div className="flex justify-around pt-4 ">
+              {dropInitiated ? (
+                <LoadingIndicatorProgress className="h-full w-full bg-black" />
+              ) : (
+                <div className="space-y-2">
+                  <p className="text-base text-blue-300">
+                    Your data is not uploaded to any server, it will stay in your local browser.
+                  </p>
+                  <p className="text-lg text-primary-active pt-6 font-semibold">
+                    Drag and Drop DICOM files here to load them in the Viewer
+                  </p>
+                </div>
+              )}
+              <div className="flex justify-around pt-4">
                 {getLoadButton(onDrop, 'Load files', false)}
                 {getLoadButton(onDrop, 'Load folders', true)}
               </div>
